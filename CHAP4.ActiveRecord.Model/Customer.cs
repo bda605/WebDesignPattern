92fd6c95df216b07561f114b668fbed0d4b7b5ca
USER <USER@DESKTOP-MQGA10Q>
2018-05-20 21:17:07 +0800

    加入ActiveRecord Pattern DataMapper Pattern

---
+++ CHAP4.ActiveRecord.Model/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHAP4.ActiveRecord.Model
+{
+    public class Customer
+    {
+      
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool IsPremiumMember { get; set; }
+       
+        public Customer(int id, string name, bool isPremiumMember)
+        {
+            ID = id;
+            Name = name;
+            IsPremiumMember = isPremiumMember;
+        }
+
+       
+    }
+}
---
+++ CHAP4.ActiveRecord.UI/Program.cs
@@ -0,0 +1,153 @@
+﻿using CHAP4.ActiveRecord.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHAP4.ActiveRecord.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    #region Data Mapper Pattern
+    class CustomerDataMapper
+    {
+        private const string CONNECTION_STRING =
+            "Data Source=(local);Initial Catalog=DesignPatterns;Integrated Security=True";
+        public static Customer GetByID(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Open();
+
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandType = CommandType.Text;
+
+                    command.CommandText = "SELECT TOP 1 * FROM [Customer] WHERE [ID] = @ID";
+                    command.Parameters.AddWithValue("@ID", id);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                  
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+
+                        string name = (string)reader["Name"];
+                        bool isPremiumMember = (bool)reader["IsPremiumMember"];
+
+                        return new Customer(id, name, isPremiumMember);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        void Save(Customer customer) {}
+
+
+        void Delete(Customer customer)
+        {
+            using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Open();
+
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandType = CommandType.Text;
+
+                    command.CommandText = "DELETE FROM [Customer] WHERE [ID] = @ID";
+                    command.Parameters.AddWithValue("@ID", customer.ID);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+    #endregion
+
+    #region ActiveRecord
+    public class Customer
+    {
+        private const string CONNECTION_STRING =
+            "Data Source=(local);Initial Catalog=DesignPatterns;Integrated Security=True";
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool IsPremiumMember { get; set; }
+
+        public Customer(int id, string name, bool isPremiumMember)
+        {
+            ID = id;
+            Name = name;
+            IsPremiumMember = isPremiumMember;
+        }
+ 
+        public static Customer GetByID(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Open();
+
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandType = CommandType.Text;
+
+                    command.CommandText = "SELECT TOP 1 * FROM [Customer] WHERE [ID] = @ID";
+                    command.Parameters.AddWithValue("@ID", id);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    // If the query returned a row, create the Customer object and return it.
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+
+                        string name = (string)reader["Name"];
+                        bool isPremiumMember = (bool)reader["IsPremiumMember"];
+
+                        return new Customer(id, name, isPremiumMember);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public void Save()
+        {
+ 
+        }
+
+        public void Delete()
+        {
+            using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Open();
+
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandType = CommandType.Text;
+
+                    command.CommandText = "DELETE FROM [Customer] WHERE [ID] = @ID";
+         
+                    command.Parameters.AddWithValue("@ID", ID);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+
+    #endregion
+
+
+}
